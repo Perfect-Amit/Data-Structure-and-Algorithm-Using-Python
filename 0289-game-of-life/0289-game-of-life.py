c06a15dbb92ed5e9a40b71346b92b315ec07fb21
Amit Vijaynath Tiwari <182439737+Perfect-Amit@users.noreply.github.com>
2026-03-31 04:34:05 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.7%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,23 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        m=len(board)
+        n=len(board[0])
+        directions=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+        for i in range(m):
+            for j in range(n):
+                live=0
+                for dx,dy in directions:
+                    x=i+dx
+                    y=j+dy
+                    if 0<=x<m and 0<=y<n and abs(board[x][y])==1:
+                        live+=1
+                if board[i][j]==1 and (live<2 or live>3):
+                    board[i][j]=-1
+                elif board[i][j]==0 and live==3:
+                    board[i][j]=2
+        for i in range(m):
+            for j in range(n):
+                if board[i][j]>0:
+                    board[i][j]=1
+                else:
+                    board[i][j]=0
